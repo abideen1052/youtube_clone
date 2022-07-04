01346088c97d5d27dd5d7b2f337538462231d47c
Mohammed Zainul Abideen <abideen1052@gmail.com>
2022-07-04 22:18:44 +0530

    Floating Appbar added and basic homepage created

--- lib/presentation/home/sceen_home.dart
+++ lib/presentation/home/sceen_home.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:ytube_clone/core/colors/colors.dart';
 import 'package:ytube_clone/core/constants/const.dart';
 import 'package:ytube_clone/presentation/home/widgets/appbar_list_item.dart';
@@ -6,70 +10,91 @@ import 'package:ytube_clone/presentation/home/widgets/video_and_details.dart';
 
 import 'package:ytube_clone/presentation/widgets/app_bar_widget.dart';
 
+ValueNotifier<bool> scrollNotifier = ValueNotifier(false);
+
 class ScreenHome extends StatelessWidget {
   const ScreenHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            backgroundColor: backgroundColor,
+            toolbarHeight: 90,
+            flexibleSpace: const AppBarMainWidget(),
+          )
+        ],
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (BuildContext ctx, int index) {
+              return const VideoAndDetailsWidget();
+            }),
+      ),
+    );
+  }
+}
+
+class AppBarMainWidget extends StatelessWidget {
+  const AppBarMainWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 90,
+      color: kTransparentColor,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: double.infinity,
-            height: 90,
-            color: kTransparentColor,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const AppBarWidget(),
-                kHeight5,
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ElevatedButton.icon(
-                          onPressed: () {},
-                          icon: const Icon(Icons.explore),
-                          label: const Text('Explore'),
-                          style: ElevatedButton.styleFrom(
-                            primary: kGreyItemColor,
-                          ),
-                        ),
-                        kwidth15,
-                        const AppBarItemWidget(size: 38, title: 'All'),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Street Food', size: 90),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Comedy', size: 70),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Music', size: 70),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Travel', size: 70),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Tourism', size: 70),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Cooking', size: 70),
-                        kwidth5,
-                        const AppBarItemWidget(title: 'Computers', size: 90),
-                        kwidth5,
-                        const Text(
-                          'SEND FEEDBACK',
-                          style: TextStyle(color: Colors.blue, fontSize: 13),
-                        ),
-                        kwidth15,
-                      ],
+          const AppBarWidget(),
+          kHeight5,
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.explore),
+                    label: const Text('Explore'),
+                    style: ElevatedButton.styleFrom(
+                      primary: kGreyItemColor,
                     ),
                   ),
-                ),
-              ],
+                  kwidth15,
+                  const AppBarItemWidget(size: 38, title: 'All'),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Street Food', size: 90),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Comedy', size: 70),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Music', size: 70),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Travel', size: 70),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Tourism', size: 70),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Cooking', size: 70),
+                  kwidth5,
+                  const AppBarItemWidget(title: 'Computers', size: 90),
+                  kwidth5,
+                  const Text(
+                    'SEND FEEDBACK',
+                    style: TextStyle(color: Colors.blue, fontSize: 13),
+                  ),
+                  kwidth15,
+                ],
+              ),
             ),
           ),
-          Expanded(child:
-              ListView.builder(itemBuilder: (BuildContext ctx, int index) {
-            return const VideoAndDetailsWidget();
-          }))
         ],
       ),
     );
